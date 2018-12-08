43f8a19bc8d885e30d3f3f720e09cb6c0f5d92a1
Vetle Økland <vetle@bordplate.no>
2018-12-08 02:21:56 +0100

    Cleans up assembly for first.s

--- assembly/first.s
+++ assembly/first.s
@@ -1,31 +1,34 @@
 BITS 64
 
 main:
-call getalert  ; push rip; jmp getalert
-int 3
+    call    getalert
+    int     3
 
 getalert:
-push rbp
-mov rbp, rsp
-mov eax, msg
-mov rdi, rax
-call alert
-mov eax, sec
-mov rdi, rax
-call alert
-pop rbp
-ret
+    push    rbp
+    mov     rbp, rsp
+    mov     eax, msg
+    mov     rdi, rax
+    call    alert
+    mov     eax, sec
+    mov     rdi, rax
+    call    alert
+    pop     rbp
+    ret
 
 alert:
-int 1
-ret
+    int     1
+    ret
 
 wowhacked:
-mov eax, hckd
-mov rdi, rax
-call alert
-ret
+    mov     eax, hckd
+    mov     rdi, rax
+    call    alert
+    ret
 
-msg: db "Hello, World!",0
-sec: db "Second message, just to test",0
-hckd: db "Oh, you sly motherfucker, you hacked it",0
+msg:
+    db "Hello, World!",0
+sec:
+    db "Second message, just to test",0
+hckd:
+    db "Oh, you sly motherfucker, you hacked it",0
