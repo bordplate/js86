ae70f7b6397f374a1cee213e8f61844614251834
Vetle Økland <vetle.okland@nagarro.no>
2019-07-16 20:01:16 +0200

    Now with support for identifying some labels, and other basic syntax highlighting

--- Emulator/Elements/CpuAssembly.js
+++ Emulator/Elements/CpuAssembly.js
@@ -1,3 +1,5 @@
+import {Metadata} from '../Processor/Metadata.js';
+
 export class CpuAssembly extends HTMLElement {
     constructor() {
         super();
@@ -22,18 +24,47 @@ export class CpuAssembly extends HTMLElement {
     }
 
     loadAssembly(assembly) {
+        let metadata = new Metadata(assembly);
+        metadata.analyze();
+
         this.innerHTML = assembly.flatMap((instruction) => {
             if (parseInt(instruction[0], 16) > this.codeSize) {
                 return;
             }
 
-            return `
+            var op_str = instruction[2];
+
+            var html = "";
+
+            let addressMetadata = metadata.informationForAddress(instruction[0]);
+
+            if (addressMetadata !== undefined) {
+                let isEntrypoint = !!(addressMetadata.entrypoint)
+
+                if (addressMetadata.label) {
+                    html += `
+                    <div tabindex="-30" role="row" class="label">
+                        <span class="address"></span>
+                        <span class="arrow"></span>
+                        <span class="${isEntrypoint ? "label-entrypoint" : "label-name"}">${addressMetadata.labelName}:</span>
+                        <span class="op_str"></span>
+                    </div>`;
+                }
+
+                if (addressMetadata.processOpStr !== undefined) {
+                    op_str = addressMetadata.processOpStr(op_str);
+                }
+            }
+
+            html += `
                         <div tabindex="-30" role="row" class="code-line" id="${this.uniqueName}-line-0x${instruction[0]}">
                             <span class="address">0x${instruction[0].toUpperCase().padStart(2, "0")}</span>
                             <span class="arrow"></span>
                             <span class="mnemonic">${instruction[1]}</span>&#9;&#9;
-                            <span class="op_str">${instruction[2]}</span>
-                        </div>`
+                            <span class="op_str">${op_str}</span>
+                        </div>`;
+
+            return html;
         }).join('\n');
     }
 }
---
+++ Emulator/Processor/Metadata.js
@@ -0,0 +1,132 @@
+export class Metadata {
+    constructor(assembly) {
+        this.assembly = assembly;
+
+        this.addressInformation = {};
+    }
+
+    informationForAddress(address) {
+        return this.addressInformation[address];
+    }
+
+    /**
+     *
+     * @param mnemonic string
+     * @returns {boolean}
+     */
+    static isJumpInstruction(mnemonic) {
+        let jumpMnemonics = [
+            "je", "jne", "jmp", "call"
+        ];
+
+        return jumpMnemonics.includes(mnemonic)
+    }
+
+    static registerNames() {
+        return [
+            'RAX', 'EAX', 'AX', 'AH', 'AL',
+            'RBX', 'EBX', 'BX', 'BH', 'BL',
+            'RCX', 'ECX', 'CX', 'CH', 'CL',
+            'RDX', 'EDX', 'DX', 'DH', 'DL',
+            'RSI', 'ESI', 'SI', 'SIL',
+            'RSP', 'ESP', 'SP', 'SPL',
+            'RBP', 'EBP', 'BP', 'BPL',
+            'RIP', 'EIP', 'IP',
+            'RDI', 'EDI', 'DI'
+        ];
+    }
+
+    processOpStr(op_str) {
+        return this.opStrFunctions.map((func) => {
+            op_str = func(op_str);
+            return op_str;
+        }).pop();
+    }
+
+    /**
+     *
+     * @returns {boolean}
+     */
+    analyze() {
+        this.assembly.forEach((instruction) => {
+            let [address, mnemonic, op_str] = instruction;
+            address = parseInt(address, 16);
+
+            let addressInformation = this.addressInformation[address.toString(16)] !== undefined ?
+                this.addressInformation[address.toString(16)] :
+                {};
+
+            addressInformation.processOpStr = this.processOpStr;
+            addressInformation.opStrFunctions = addressInformation.opStrFunctions instanceof Array ?
+                addressInformation.opStrFunctions :
+                [];
+
+            addressInformation.opStrFunctions.push((op_str) => {
+                Metadata.registerNames().forEach((registerName) => {
+                    op_str = op_str.replace(
+                        registerName.toLowerCase(),
+                        registerName.toUpperCase()
+                    )
+                });
+
+                Metadata.registerNames().forEach((registerName) => {
+                    op_str = op_str.replace(
+                        registerName.toUpperCase(),
+                        `<span class="op_str-register">${registerName.toLowerCase()}</span>`
+                    )
+                });
+
+                return op_str;
+            });
+
+            if (address === 0) {
+                addressInformation.label = true;
+                addressInformation.labelName = "entrypoint";
+                addressInformation.entrypoint = true;
+            }
+
+
+
+            if (Metadata.isJumpInstruction(mnemonic)) {
+                let destination = parseInt(op_str, 16);
+
+                let destinationAddressInformation = this.addressInformation[destination.toString(16)] !== undefined ?
+                    this.addressInformation[destination.toString(16)] :
+                    {};
+
+                destinationAddressInformation.label = true;
+                destinationAddressInformation.labelName = `loc_${destination.toString(16)}`;
+
+                addressInformation.processOpStr = this.processOpStr;
+                addressInformation.opStrFunctions = addressInformation.opStrFunctions instanceof Array ?
+                    addressInformation.opStrFunctions :
+                    [];
+
+                addressInformation.opStrFunctions.push((op_str) => {
+                    return op_str.replace(
+                            "0x" + destination.toString(16),
+                            `<span class="op_str-label">${destinationAddressInformation.labelName}</span>`
+                        );
+                });
+
+                this.addressInformation[destination.toString(16)] = destinationAddressInformation;
+            }
+
+            addressInformation.opStrFunctions.push((op_str) => {
+                let components = op_str.split(" ");
+
+                components.forEach((component) => {
+                    if (parseInt(component)) {
+                        op_str = op_str.replace(component, `<span class="op_str-number">${component}</span>`);
+                    }
+                });
+
+                return op_str;
+            });
+
+            this.addressInformation[address.toString(16)] = addressInformation;
+        });
+
+        return true;
+    }
+}
